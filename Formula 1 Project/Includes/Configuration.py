bfa2564deeb36e8de62b94a31cfd4a3fdc9edd7a
ashafin372@gmail.com <ashafin372@gmail.com>
2024-06-21 19:47:20 +0000

    Includes and Mounts

---
+++ Formula 1 Project/Includes/Configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/f1datalakepractice/raw"
+processed_folder_path = "/mnt/f1datalakepractice/processed"
+presentation_folder_path = "/mnt/f1datalakepractice/presentation"
+
+# COMMAND ----------
+
+
